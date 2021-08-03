357c2db25395a97405c3543ff96796875e4f9962
Emmett Rushin <emmettrushin@gmail.com>
2021-08-03 19:46:28 -0400

    added api routes

---
+++ routes/api_routes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router(); 
+const db = require('../models'); 
+
+router.post('/api/workouts', ({ body }, res) => {
+    db.Workout.create(body)
+    .then(dbWorkout => { res.json(dbWorkout) } )
+    .catch(err => { res.json(err) });
+});
+
+router.put('/api/workouts/:id', (req, res) => {
+    db.Workout.findByIdAndUpdate(
+        { _id: req.params.id }, 
+        { $push: { exercises: req.body } }, 
+        { new: true, runValidators: true }
+    )
+    .then(dbWorkout => { res.json(dbWorkout); })
+    .catch(err => { res.json(err); });
+});
+
+router.get('/api/workouts/range', (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkouts => {
+        const workouts = dbWorkouts.map(workout => {
+            console.log(workout);
+            const duration = workout.exercises.reduce((acc,next) => {
+                return acc + next.duration;
+            }, 0);
+
+            console.log("durationnnnnnnnn", duration)
+
+            return {
+                
+                totalDuration: duration, 
+                ...workout.toObject()
+            }
+        })
+        res.json(workouts); 
+    })
+    .catch(err => {
+        res.json(err); 
+    });
+});
+
+router.get('/api/workouts', (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout => { res.json(dbWorkout); })
+    .catch(err => { res.json(err); })
+});
+
+module.exports = router
